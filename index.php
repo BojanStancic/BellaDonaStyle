f8f4fdb90a5dde253abc986d89997760213bc4a0
Bojan Stancic <bojanstancic3@gmail.com>
2021-01-17 13:34:49 +0100

    updatetd

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php 
 
-// function redirect($location) {
-// 	header("Location: {$location}");
-// }
-
-// redirect('pages/pocetna.php');
-
-// header("Location: pages/pocetna.php");
 
 include ('includes/functions.php');
 
